ba84a8c51e643e8416084d78c09b79d617257ae1
Christian <christianotarmaso@gmail.com>
2022-06-20 14:42:46 +0200

    kithenupdate

--- SQL/sql1queries.sql
+++ SQL/sql1queries.sql
@@ -1,4 +1,1 @@
-use beeritup;
-
-SELECT t2.uName as name, COUNT(*) as count FROM Beverages t1 JOIN Users t2 ON t1.beverageDrinkerId = t2.id 
-WHERE kitchenId = 1 and settleDate BETWEEN '2022-06-15' AND LAST_DAY('2022-06-0') GROUP BY t2.uName ORDER BY count DESC
+use beeritup_db;
