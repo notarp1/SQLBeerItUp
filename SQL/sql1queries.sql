0790ddeb5dcc899dd4757dfbc422d128ba4f2062
Christian <christianotarmaso@gmail.com>
2022-06-20 11:20:12 +0200

    Fixed leaderboards

--- SQL/sql1queries.sql
+++ SQL/sql1queries.sql
@@ -1,11 +1,4 @@
 use beeritup;
 
-DROP TABLE BeverageTypes;
-
-DROP TABLE Beverages;
-
-DROP TABLE KitchenUsers;
-
-DROP TABLE Kitchens;
-
-DROP TABLE Users;
+SELECT t2.uName as name, COUNT(*) as count FROM Beverages t1 JOIN Users t2 ON t1.beverageDrinkerId = t2.id 
+WHERE kitchenId = 1 and settleDate BETWEEN '2022-06-15' AND LAST_DAY('2022-06-0') GROUP BY t2.uName ORDER BY count DESC
